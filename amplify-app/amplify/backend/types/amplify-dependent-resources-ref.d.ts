e0b78039ca5a890c8f42093c5ae31b6b9560faae
psxchi <65168787+psxchi@users.noreply.github.com>
2022-09-27 23:20:43 +0200

    Revert "Revert "Revert "not planning on updating backend anymore"""
    This reverts commit e41799eb587ffc808bb658eff2a7b808cee073c0.

---
+++ amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "Simpleblog": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "Simpleblog": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
